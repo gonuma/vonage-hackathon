b416925833e0ebb1a729dec16c10995ecfc2c1d8
gonuma <81454703+gonuma@users.noreply.github.com>
2021-09-14 20:47:35 +0900

    Will multiple video sources work now?

--- src/features/video/Video.js
+++ src/features/video/Video.js
@@ -80,6 +80,7 @@ export default class App extends React.Component {
     const { error, connection, publishVideo } = this.state;
     return (
       <div>
+        {console.log(token)}
         <div id="sessionStatus">Session Status: {connection}</div>
         {error ? (
           <div className="error">
--- video.js
+++ video.js
@@ -1,23 +1,24 @@
-// const VonageVideo = require("opentok");
-// const vonageVideo = new VonageVideo(apiKey, apiSecret);
+const VonageVideo = require("opentok");
+const vonageVideo = new VonageVideo(apiKey, apiSecret);
 
-// // vonageVideo.createSession((err, session) => {
-// //   console.log(session.sessionId);
-// // });
-
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const app = express();
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(express.static("public"));
-
-// app.get("/token", (req, res) => {
-//   res.json({
-//     apiKey,
-//     sessionId,
-//     token: vonageVideo.generateToken(sessionId),
-//   });
+// vonageVideo.createSession((err, session) => {
+//   console.log(session.sessionId);
 // });
 
-// app.listen(3000);
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+// const token = vonageVideo.generateToken(sess)
+app.get("/token", (req, res) => {
+  res.json({
+    apiKey,
+    sessionId,
+    token: vonageVideo.generateToken(sessionId),
+  });
+});
+
+app.listen(3000);
