1e4bfd003f4fbc4489850b1f20b4c4b911978390
taku95 <takuyay95@gamil.com>
2021-09-15 15:33:50 +0900

    added components

--- src/features/workspace/Workspace.js
+++ src/features/workspace/Workspace.js
@@ -1,28 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { makeStyles } from '@material-ui/core/styles';
-import { yellow } from '@material-ui/core/colors';
-
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import { yellow } from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme) => ({
   workspace: {
-    width:"60%",
-    height:"40rem"
+    width: "60%",
+    height: "40rem",
   },
   doc: {
-    width:"100%",
-    height:"100%"
-  }
+    width: "100%",
+    height: "100%",
+  },
 }));
 
 export function Workspace() {
   const classes = useStyles();
-  const files = useSelector((state) => state.files)
-  
+  const files = useSelector((state) => state.files);
+
   return (
     <div className={classes.workspace}>
-      {files.currentSelected ? <iframe src={`https://docs.google.com/document/d/1NhNv4DdTXERbkZfJJQUvFxcGYD62diFyuBfh1JDOLC4/edit?usp=sharing`} className={classes.doc}></iframe> : null}
+      {files.currentSelected ? (
+        <iframe
+          src={`https://docs.google.com/document/d/1NhNv4DdTXERbkZfJJQUvFxcGYD62diFyuBfh1JDOLC4/edit?usp=sharing`}
+          className={classes.doc}
+        ></iframe>
+      ) : null}
     </div>
-  )
-  
+  );
 }
