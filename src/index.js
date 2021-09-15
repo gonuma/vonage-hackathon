0b706e811c84164f2d516e456acd4d0eaa7549ae
GiveKumaAGun <reijitsusaka@gmail.com>
2021-09-15 12:23:53 +0900

    Reverting to 0ab44c8d7c20c8519a60edbc7fe181dfd29e8dce

--- src/index.js
+++ src/index.js
@@ -1,41 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "@opentok/client";
-
-import App from "./App";
-import { store } from "./app/store";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 // import * as serviceWorker from './serviceWorker';
-import { SERVER_BASE_URL, API_KEY, SESSION_ID, TOKEN } from "./config";
 
-function renderApp(credentials) {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App credentials={credentials} />
-      </Provider>
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
-}
-
-if (API_KEY && TOKEN && SESSION_ID) {
-  renderApp({
-    apiKey: API_KEY,
-    sessionId: SESSION_ID,
-    token: TOKEN,
-  });
-} else {
-  fetch(SERVER_BASE_URL + "/session")
-    .then((data) => data.json())
-    .then(renderApp)
-    .catch((err) => {
-      console.error("Failed to get session credentials", err);
-      alert(
-        "Failed to get opentok sessionId and token. Make sure you have updated the config.js file."
-      );
-    });
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
