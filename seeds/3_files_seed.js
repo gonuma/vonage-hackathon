724f460500c605caef671bad73a65de7918a9f9a
gonuma <81454703+gonuma@users.noreply.github.com>
2021-09-17 16:18:04 +0900

    Slight CSS fix on landing page

--- seeds/3_files_seed.js
+++ seeds/3_files_seed.js
@@ -8,25 +8,29 @@ exports.seed = function (knex) {
         {
           id: 1,
           name: "Test File Please Ignore",
-          srcstring: "https://docs.google.com/presentation/d/1jg8FyX7Kz-J-K2OJ9c8fXcfMAhJ6xaEnjs_fWQ8HhO8/edit?usp=sharing",
+          srcstring:
+            "https://docs.google.com/presentation/d/1jg8FyX7Kz-J-K2OJ9c8fXcfMAhJ6xaEnjs_fWQ8HhO8/edit?usp=sharing",
           workspaceId: 1,
         },
         {
           id: 2,
           name: "Is this a spreadsheet?",
-          srcstring: "https://docs.google.com/spreadsheets/d/1TFHuNuRoDBOgONjvKp0G3z-zLoJjPwn9P9HGskLxsRU/edit?usp=sharing",
+          srcstring:
+            "https://docs.google.com/spreadsheets/d/1TFHuNuRoDBOgONjvKp0G3z-zLoJjPwn9P9HGskLxsRU/edit?usp=sharing",
           workspaceId: 1,
         },
         {
           id: 3,
           name: "Doc",
-          srcstring: "https://docs.google.com/spreadsheets/d/1TFHuNuRoDBOgONjvKp0G3z-zLoJjPwn9P9HGskLxsRU/edit?usp=sharing",
+          srcstring:
+            "https://docs.google.com/spreadsheets/d/1TFHuNuRoDBOgONjvKp0G3z-zLoJjPwn9P9HGskLxsRU/edit?usp=sharing",
           workspaceId: 2,
         },
         {
           id: 4,
           name: "Doc",
-          srcstring: "https://docs.google.com/spreadsheets/d/1TFHuNuRoDBOgONjvKp0G3z-zLoJjPwn9P9HGskLxsRU/edit?usp=sharing",
+          srcstring:
+            "https://docs.google.com/spreadsheets/d/1TFHuNuRoDBOgONjvKp0G3z-zLoJjPwn9P9HGskLxsRU/edit?usp=sharing",
           workspaceId: 3,
         },
       ]);
